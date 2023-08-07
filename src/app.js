6a105e0650517acfbcb2dc0a76abb5bdb16daf6c
legt <legt1407@gmail.com>
2023-08-07 15:30:31 -0400

    testing cors

--- src/app.js
+++ src/app.js
@@ -8,11 +8,13 @@ config();
 
 const port = process.env.PORT;
 const app = express();
-const server = http.createServer(app.use);
+const server = http.createServer(app.use(cors()));
 const io = new Server(server, {
+  allowEIO3: true,
   cors: {
-    origin: "*"
-  }
+    origin: [process.env.ORIGIN, process.env.ORIGIN2, process.env.ORIGIN3],
+    methods: ["GET", "POST"],
+  },
 });
 
 
